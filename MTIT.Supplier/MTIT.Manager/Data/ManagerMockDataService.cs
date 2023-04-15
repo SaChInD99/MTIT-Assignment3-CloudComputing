38dbc6799159c09a42190972df5919d9d400d5f4
Sachin <sachindileepa41@gmail.com>
2023-04-15 12:18:39 +0530

    implement

--- MTIT.Supplier/MTIT.Manager/Data/ManagerMockDataService.cs
+++ MTIT.Supplier/MTIT.Manager/Data/ManagerMockDataService.cs
@@ -6,11 +6,11 @@ namespace MTIT.Manager.Data
     {
         public static List<Models.Manager> Managers = new List<Models.Manager>()
         {
-            new Models.Manager { Id = 1, ItemName ="Bat",ItemQuantity ="10",ItemPrice =200000},
-            new Models.Manager { Id = 2, ItemName ="Ball",ItemQuantity ="40",ItemPrice =22000},
-            new Models.Manager { Id = 3, ItemName ="helmet",ItemQuantity ="78",ItemPrice =21000},
-            new Models.Manager { Id = 4, ItemName ="sportswear ",ItemQuantity ="32",ItemPrice =23000},
-            new Models.Manager { Id = 5, ItemName ="footwear",ItemQuantity ="65",ItemPrice =19000}
+            new Models.Manager { Id = 1, ItemName ="Dialog Broadband",ItemQuantity ="15",ItemPrice =200000},
+            new Models.Manager { Id = 2, ItemName ="Dialog TV",ItemQuantity ="40",ItemPrice =22000},
+            new Models.Manager { Id = 3, ItemName ="Dialog Phone",ItemQuantity ="78",ItemPrice =21000},
+            new Models.Manager { Id = 4, ItemName ="Dialog Satellite ",ItemQuantity ="32",ItemPrice =23000},
+            new Models.Manager { Id = 5, ItemName ="Dialog Adapter",ItemQuantity ="65",ItemPrice =19000}
 
         };
 
--- MTIT.Supplier/MTIT.Ocelote/Program.cs
+++ MTIT.Supplier/MTIT.Ocelote/Program.cs
@@ -1,6 +1,12 @@
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot();
 var app = builder.Build();
 
 app.MapGet("/", () => "Successfuly Launched Dialog Company API");
+app.UseOcelot();
 
 app.Run();
